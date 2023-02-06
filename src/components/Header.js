6eb26dcb6d7b9f8c90c05260fbb779b19404344c
sahillather002 <sahillather002@gmail.com>
2023-02-06 17:25:35 +0530

    header updated

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Header.css'
+import DescriptionOutlinedIcon from '@mui/icons-material/DescriptionOutlined';
+import { green } from '@mui/material/colors';
+export default function Header() {
+  return (
+    
+    <div className='Header'>
+        <div>
+            <DescriptionOutlinedIcon 
+            sx={{
+                color:"blue" ,
+                fontSize:"100px",
+                padding:"0 50px",
+                "&:hover": {color: green[500]},
+                }}
+            />
+        </div>
+        <div class="Heading">
+            
+            <h2 >Notes Keeper</h2>
+      </div>
+    </div>
+  )
+}
