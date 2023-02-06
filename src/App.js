55ce6f8e5db7d99e8e7612dd53b43758081aa215
sahillather002 <sahillather002@gmail.com>
2023-02-06 22:13:19 +0530

    note useState implemented and deleteNote

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { useState } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import CreateArea from "./components/CreateArea";
+import Note from "./Components/Note";
 function App(){
+  const[notes,setNotes] = useState([]);
+  function addNote(newNote){
+    setNotes((prevNotes) =>{
+      return [...prevNotes,newNote];
+    })
+  }
+
+  function deleteNote(id){
+    setNotes((prevNotes) => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      })
+    })
+  }
   return (
     <div>
       <Header />
