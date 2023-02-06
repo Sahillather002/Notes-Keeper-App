936c2d8b9c627fd56818f5b8799a1cec3b3138cf
sahillather002 <sahillather002@gmail.com>
2023-02-06 22:08:52 +0530

    footer css update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,14 @@
 import React from "react";
+import './Footer.css'
 
 function Footer() {
   const year = new Date().getFullYear();
   return (
-    <footer>
-      <p>Copyright ⓒ {year}</p>
-    </footer>
+    
+      <div className="footer">
+      Copyright ⓒ {year}
+      </div>
+    
   );
 }
 
