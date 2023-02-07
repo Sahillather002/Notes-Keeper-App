90f1c86a50b8ed01f0ed4e372e6cd9e55e57c575
sahillather002 <sahillather002@gmail.com>
2023-02-07 08:04:28 +0530

    notes css updated

--- src/components/Note.js
+++ src/components/Note.js
@@ -7,7 +7,7 @@ function Note(props) {
   }
 
   return (
-    <div className="Note">
+    <div className="note">
       <h1>{props.title}</h1>
       <p>{props.content}</p>
       <button onClick={handleClick}>
